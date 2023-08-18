43a484b4b1776093816b323b2ab5268ec6cda9d9
FlavioT04 <flavio.mendotri@gmail.com>
2023-08-17 22:05:29 -0500

    Changed models, removed some code, and added HTML

--- mysite/macrosweb/admin.py
+++ mysite/macrosweb/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Food_display
 
-# Register your models here.
+admin.site.register(Food_display)
--- mysite/macrosweb/models.py
+++ mysite/macrosweb/models.py
@@ -1,22 +1,14 @@
 from django.db import models
 
 class Food_display(models.Model):
-    food_name = models.CharField(max_length=30)
-    f_protein = models.DecimalField(max_digits=5, decimal_places=2)
-    f_carbs = models.DecimalField(max_digits=5, decimal_places=2)
-    f_fat = models.DecimalField(max_digits=5, decimal_places=2)
-    f_calories = models.DecimalField(max_digits=6, decimal_places=2) # Use method compute calories
+
+    name = models.CharField(max_length=30)
+    protein = models.DecimalField(max_digits=5, decimal_places=2)
+    carbs = models.DecimalField(max_digits=5, decimal_places=2)
+    fat = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
 
     def add_food():
         pass
-
-class Stats(models.Model):
-    food_id = models.ForeignKey(Food_display, on_delete=models.CASCADE)
-    total_protein = models.DecimalField(max_digits=5, decimal_places=2)
-    total_carbs = models.DecimalField(max_digits=5, decimal_places=2)
-    total_fat = models.DecimalField(max_digits=5, decimal_places=2)
-    total_calories = models.DecimalField(max_digits=6, decimal_places=2) # Use method compute calories
-
-# Calculates calories in food
-def compute_calories(protein, carbs, fat):
-    return protein*4 + carbs*4 + fat*9
